39d3f184634aaa6729690043cb8069e61bb3d4e8
Scott Cote <scott.cote@outlook.com>
2023-01-04 13:24:58 -0500

    WIP

--- bin/qbXMLHandler/index.js
+++ bin/qbXMLHandler/index.js
@@ -11,13 +11,39 @@
  * file that was distributed with this source code.
  */
 
+var uuid = require('node-uuid');
 var data2xml = require('data2xml');
 var convert = data2xml({
         xmlHeader: '<?xml version="1.0" encoding="utf-8"?>\n<?qbxml version="16.0"?>\n'
     });
 
+var ticketMap = new Map();
+
 // Public
 module.exports = {
+
+    authenticate: async function(username, password) {
+        if (username === 'username' && password === 'password') {
+            let ticketId = uuid.v1();
+            ticketMap.set(ticketId, { ticketId });
+            return { ticketId, companyFilePath: '' }
+        } else {
+           throw new Error('Invalid username or password.');
+        }
+    },
+
+    validateTicket: async function(ticketId) {
+        return ticketMap.has(ticketId);
+    },
+
+    setError: async function(ticketId, error) {
+        let ticket = ticketMap.get(ticketId);
+        if (ticket) ticket.error = error.toString();
+    },
+
+    getError: async function(ticketId) {
+        return ticketMap.get(ticketId)
+    },
 
     /**
      * Builds an array of qbXML commands
@@ -25,8 +51,40 @@ module.exports = {
      *
      * @param callback(err, requestArray)
      */
-    fetchRequests: function(callback) {
-        buildRequests(callback);
+    fetchRequest: function(callback) {
+        console.log('fetchRequest');
+        var xml;
+        if (!iteratorId) {
+            xml = convert(
+                'QBXML',
+                {
+                    QBXMLMsgsRq : {
+                        _attr : { onError : 'stopOnError' },
+                        CustomerQueryRq : {
+                            _attr: { iterator: "Start" },
+                            MaxReturned: 2,
+                        },
+                    },
+                }
+            );
+        } else {
+            xml = null;
+            /*
+            xml = convert(
+                'QBXML',
+                {
+                    QBXMLMsgsRq : {
+                        _attr : { onError : 'stopOnError' },
+                        CustomerQueryRq : {
+                            _attr: { iterator: "Continue" },
+                            MaxReturned: 2,
+                        },
+                    },
+                }
+            );
+            */
+        }
+        callback(null, xml);
     },
 
     /**
@@ -37,16 +95,12 @@ module.exports = {
      */
     handleResponse: function(response) {
         console.log(response);
+        iteratorId = 'fake';
+        return 50;
     },
 
-    /**
-     * Called when there is an error
-     * returned processing qbXML from QBWC.
-     *
-     * @param error - qbXML error response
-     */
-    didReceiveError: function(error) {
-        console.log(error);
+    closeTicket: async function(ticketId) {
+        ticketMap.delete(ticketId);
     }
 };
 
